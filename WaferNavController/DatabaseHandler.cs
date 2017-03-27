942917182e1ae10d1c7dfca12ef4e0c2480bd126
Brian Chipman <namedtuple@gmail.com>
2017-03-26 23:33:10 -0700

    Minor refactoring of DatabaseHandler class for consistency.

--- WaferNavController/DatabaseHandler.cs
+++ WaferNavController/DatabaseHandler.cs
@@ -6,36 +6,36 @@ namespace WaferNavController {
 
     class DatabaseHandler {
 
-        private static SqlConnection myConnection;
+        private static SqlConnection connection;
 
         public static void ConnectToDatabase() {
-            myConnection = new SqlConnection(
+            connection = new SqlConnection(
                 "user id=appuser;" +
                 "password=appuser;" +
                 "server=localhost;" +
                 "database=wafer_nav;" +
                 "connection timeout=3");
-            myConnection.Open();
+            connection.Open();
         }
 
         public static List<Dictionary<string, string>> GetAllBlus() {
-            return GetData("SELECT * FROM [wn].[BLU]");
+            return GetData("SELECT * FROM [wn].[BLU];");
         }
 
         public static Dictionary<string, string> GetBlu(string bluId) {
-            return GetData($"SELECT * FROM [wn].[BLU] WHERE id = {bluId}")[0];
+            return GetData($"SELECT * FROM [wn].[BLU] WHERE id = {bluId};")[0];
         }
 
         public static List<Dictionary<string, string>> GetAllActiveBibs() {
-            return GetData("SELECT * FROM [wn].[active_bib]");
+            return GetData("SELECT * FROM [wn].[active_bib];");
         }
 
         public static List<Dictionary<string, string>> GetAllHistoricBibs() {
-            return GetData("SELECT * FROM [wn].[historic_bib]");
+            return GetData("SELECT * FROM [wn].[historic_bib];");
         }
 
         private static List<Dictionary<string, string>> GetData(string query) {
-            var sqlCommand = new SqlCommand(query, myConnection);
+            var sqlCommand = new SqlCommand(query, connection);
             var reader = sqlCommand.ExecuteReader();
 
             var data = new List<Dictionary<string, string>>();
@@ -57,7 +57,7 @@ namespace WaferNavController {
 
         public static string GetFirstAvailableBluId() {
             var query = "SELECT * FROM [wn].[BLU] WHERE available = 1;";
-            var sqlCommand = new SqlCommand(query, myConnection);
+            var sqlCommand = new SqlCommand(query, connection);
             var reader = sqlCommand.ExecuteReader();
 
             string bluId = null;
@@ -69,39 +69,39 @@ namespace WaferNavController {
             return bluId;
         }
 
-        public static void SetAllBluToUnavailable() {
-            var query = "UPDATE[wafer_nav].[wn].[BLU]" +
-                        "SET available = 0";
-            var updateCommand = new SqlCommand(query, myConnection);
-            updateCommand.ExecuteNonQuery();
-        }
-
         public static void SetAllBluToAvailable() {
             var query = "UPDATE[wafer_nav].[wn].[BLU]" +
-                        "SET available = 1";
-            var updateCommand = new SqlCommand(query, myConnection);
+                        "SET available = 1;";
+            var updateCommand = new SqlCommand(query, connection);
             updateCommand.ExecuteNonQuery();
         }
 
-        public static void SetBluToUnavailable(string bluId) {
+        public static void SetAllBluToUnavailable() {
             var query = "UPDATE[wafer_nav].[wn].[BLU]" +
-                        "SET available = 0" +
-                        $"WHERE id = {bluId}";
-            var updateCommand = new SqlCommand(query, myConnection);
+                        "SET available = 0;";
+            var updateCommand = new SqlCommand(query, connection);
             updateCommand.ExecuteNonQuery();
         }
 
         public static void SetBluToAvailable(string bluId) {
             var query = "UPDATE[wafer_nav].[wn].[BLU]" +
                         "SET available = 1" +
-                        $"WHERE id = {bluId}";
-            var updateCommand = new SqlCommand(query, myConnection);
+                        $"WHERE id = {bluId};";
+            var updateCommand = new SqlCommand(query, connection);
+            updateCommand.ExecuteNonQuery();
+        }
+
+        public static void SetBluToUnavailable(string bluId) {
+            var query = "UPDATE[wafer_nav].[wn].[BLU]" +
+                        "SET available = 0" +
+                        $"WHERE id = {bluId};";
+            var updateCommand = new SqlCommand(query, connection);
             updateCommand.ExecuteNonQuery();
         }
 
         public static void AddNewActiveBib(string bibId) {
             try {
-                var insertCommand = new SqlCommand($"INSERT INTO [wn].[active_bib] (id) Values ({bibId})", myConnection);
+                var insertCommand = new SqlCommand($"INSERT INTO [wn].[active_bib] (id) Values ({bibId});", connection);
                 insertCommand.ExecuteNonQuery();
             }
             catch (Exception exception) {
@@ -111,28 +111,28 @@ namespace WaferNavController {
 
         public static void MoveActiveBibToHistoricBib(string bibId) {
             var query = $"DELETE FROM[wn].[active_bib] where id = {bibId};";
-            var command = new SqlCommand(query, myConnection);
+            var command = new SqlCommand(query, connection);
             command.ExecuteNonQuery();
 
             query = $"INSERT INTO [wn].[historic_bib] (id) Values ({bibId});";
-            command = new SqlCommand(query, myConnection);
+            command = new SqlCommand(query, connection);
             command.ExecuteNonQuery();
         }
 
         public static void RemoveAllActiveBibs() {
             var query = "DELETE FROM [wn].[active_bib];";
-            var deleteCommand = new SqlCommand(query, myConnection);
+            var deleteCommand = new SqlCommand(query, connection);
             deleteCommand.ExecuteNonQuery();
         }
 
         public static void RemoveAllHistoricBibs() {
             var query = "DELETE FROM [wn].[historic_bib];";
-            var deleteCommand = new SqlCommand(query, myConnection);
+            var deleteCommand = new SqlCommand(query, connection);
             deleteCommand.ExecuteNonQuery();
         }
 
         public static void CloseConnection() {
-            myConnection.Close();
+            connection.Close();
         }
     }
 }
