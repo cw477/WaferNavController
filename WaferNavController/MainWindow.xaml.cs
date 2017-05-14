759521df49253766a00019eac2fd760a94bed790
Brian Chipman <namedtuple@gmail.com>
2017-05-13 23:47:33 -0700

    Moved MQTT connection logic from MainWindow to new MqttConnectionHandler class.  Also now allow pressing 's' to reset database on status tab as well as log tab.

--- WaferNavController/MainWindow.xaml.cs
+++ WaferNavController/MainWindow.xaml.cs
@@ -1,30 +1,19 @@
 using System;
 using System.Collections.Generic;
-using System.Text;
 using System.Threading;
 using System.Windows;
 using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using Newtonsoft.Json;
-using uPLibrary.Networking.M2Mqtt;
-using uPLibrary.Networking.M2Mqtt.Messages;
 using System.Windows.Input;
-using System.Configuration;
-using System.Data;
-using System.Data.SqlClient;
 using System.Threading.Tasks;
 using System.Windows.Controls;
 
 namespace WaferNavController {
     public partial class MainWindow : Window {
-        private readonly string BROKER_URL = "iot.eclipse.org"; // Defaults to port 1883
-        private readonly string CLIENT_ID = Guid.NewGuid().ToString();
-        private readonly string PUB_TOPIC = "wafernav/location_data";
-        private readonly string SUB_TOPIC = "wafernav/location_requests";
-        private readonly MqttClient mqttClient;
         private bool killMakeDotsThread = false;
-        private Config configPage;
+        private readonly MqttConnectionHandler mqttConnectionHandler;
+        private readonly Config configPage;
 
         public MainWindow() {
             InitializeComponent();
@@ -36,82 +25,8 @@ namespace WaferNavController {
             ImageSource wpfBitmap = Imaging.CreateBitmapSourceFromHBitmap(hBitmap, IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
             Icon = wpfBitmap;
 
-            AppendLine("ClIENT ID: " + CLIENT_ID);
-
             //TODO: move mqtt logic to app.xaml.cs hopefully possibly. The idea being this main window won't always be active.
-            mqttClient = new MqttClient(BROKER_URL);
-            mqttClient.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
-            mqttClient.Connect(CLIENT_ID);
-            mqttClient.Subscribe(new[] {SUB_TOPIC}, new[] {MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE});
-            AppendLine("Subscribed to " + SUB_TOPIC);
-        }
-
-        private void fillDataGrids() {
-            DatabaseHandler.fillItems(ref configPage.dgBLU, "BLU");
-            DatabaseHandler.fillItems(ref configPage.dgSLT, "SLT");
-            configPage.lastRefreshedLabel.Content = " Last refreshed: " + DateTime.Now;
-        }
-
-        /// <summary>
-        /// This method directs incoming mqtt messages to be further processed.
-        /// </summary>
-        /// <param name="directive">Enum-like string that directs what to do with messages.</param>
-        /// <param name="messages">Contents of message.</param>
-        private Dictionary<string, string> incomingMessageProcessor(Dictionary<String, object> messages)
-        {
-            Dictionary<string, string> returnMessage = null;
-            try {
-                switch ((string) messages["directive"]) {
-                    case "GET_NEW_BLU":
-                        returnMessage = NavigationHandler.getNewBlu(messages);
-                        break;
-                    case "COMPLETE_NEW_BLU":
-                        returnMessage = NavigationHandler.completeNewBlu(messages);
-                        break;
-                    case "GET_NEW_SLT":
-                        returnMessage = NavigationHandler.getNewSlt(messages);
-                        break;
-                    case "COMPLETE_NEW_SLT":
-                        returnMessage = NavigationHandler.completeNewSlt(messages);
-                        break;
-                    case "GET_DONE_BLU":
-                        returnMessage = NavigationHandler.getDoneBlu(messages);
-                        break;
-                    case "COMPLETE_DONE_BLU":
-                        returnMessage = NavigationHandler.completeDoneBlu(messages);
-                        break;
-                    default:
-                        Console.Error.WriteLine("incomingMessageProcessor: Directive unrecognized.");
-                        break;
-                }
-            }
-            catch (Exception e) {
-                returnMessage = new Dictionary<string, string> {["error"] = e.Message};
-            }
-            return returnMessage;
-        }
-
-        private void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e) {
-            var incommingJson = JsonConvert.DeserializeObject<Dictionary<string, object>>(Encoding.UTF8.GetString(e.Message, 0, e.Message.Length));
-
-            // Print received message to window
-            Dispatcher.Invoke(() => {
-                configPage.logBox.Text += DateTime.Now + "  Message arrived.  Topic: " + e.Topic + "  Message: '"
-                    + DatabaseHandler.jsonToStr(incommingJson) + "\n\n";
-                configPage.scrollViewer.ScrollToVerticalOffset(double.MaxValue);
-            });
-
-            var returnJson = incomingMessageProcessor(incommingJson);
-            returnJson["computerName"] = Environment.MachineName;
-
-            // Print outgoing message to window
-            Dispatcher.Invoke(() => {
-                configPage.logBox.Text += DateTime.Now + "  Message outgoing:" + DatabaseHandler.jsonToStr(returnJson) + "\n\n";
-                configPage.scrollViewer.ScrollToVerticalOffset(double.MaxValue);
-            });
-
-            // Publish return message
-            mqttClient.Publish(PUB_TOPIC, Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(returnJson)));
+            mqttConnectionHandler = new MqttConnectionHandler(this);
         }
 
         protected override void OnContentRendered(EventArgs e) {
@@ -128,26 +43,33 @@ namespace WaferNavController {
         }
 
         private void MainWindow_KeyDown(object sender, KeyEventArgs e) {
+            string currentTabName = ((TabItem)configPage.TabControl.SelectedItem).Name;
             if (e.Key == Key.A)
             {
-                if (((TabItem) configPage.TabControl.SelectedItem).Name == "LogTabItem") {
+                if (currentTabName == "LogTabItem") {
                     AppendCurrentDatabaseData();
                 }
             }
             if (e.Key == Key.S)
             {
-                if (((TabItem) configPage.TabControl.SelectedItem).Name == "LogTabItem") {
-                    AppendLine(DateTime.Now.ToString() + ": Resetting DB...", true);
+                if (currentTabName == "LogTabItem" || currentTabName == "StatusTabItem") {
+                    AppendLine(DateTime.Now + ": Resetting DB...", true);
                     DatabaseHandler.ResetDatabase();
-                    AppendLine(DateTime.Now.ToString() + ": Resetting DB Finished.", true);
+                    AppendLine(DateTime.Now + ": Resetting DB Finished.", true);
                 }
             }
             if (e.Key == Key.D)
             {
-                if (((TabItem) configPage.TabControl.SelectedItem).Name == "StatusTabItem") {
+                if (currentTabName == "StatusTabItem") {
                     fillDataGrids();
                 }
             }
+        }
+
+        private void fillDataGrids() {
+            DatabaseHandler.fillItems(ref configPage.dgBLU, "BLU");
+            DatabaseHandler.fillItems(ref configPage.dgSLT, "SLT");
+            configPage.lastRefreshedLabel.Content = " Last refreshed: " + DateTime.Now;
         }
 
         private void ConnectToDatabase() {
@@ -250,7 +172,7 @@ namespace WaferNavController {
             }
         }
 
-        private void AppendLine(string text, bool useDispatcher) {
+        public void AppendLine(string text, bool useDispatcher) {
             if (useDispatcher) {
                 Dispatcher.Invoke(() => AppendLine(text));
             } else {
@@ -270,7 +192,7 @@ namespace WaferNavController {
 
         protected override void OnClosed(EventArgs e) {
             base.OnClosed(e);
-            mqttClient.Disconnect();
+            mqttConnectionHandler.Disconnect();
             Application.Current.Shutdown();
         }
     }
---
+++ WaferNavController/MqttConnectionHandler.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+namespace WaferNavController {
+    class MqttConnectionHandler {
+        private readonly string BROKER_URL = "iot.eclipse.org"; // Defaults to port 1883
+        public string ClientId { get; } = Guid.NewGuid().ToString();
+        public string PubTopic { get; } = "wafernav/location_data";
+        public string SubTopic { get; } = "wafernav/location_requests";
+        private readonly MqttClient mqttClient;
+        private readonly MainWindow mainWindow;
+
+        public MqttConnectionHandler(MainWindow mainWindow) {
+            this.mainWindow = mainWindow;
+            mqttClient = new MqttClient(BROKER_URL);
+            mqttClient.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
+            mqttClient.Connect(ClientId);
+            mqttClient.Subscribe(new[] { SubTopic }, new[] { MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE });
+            mainWindow.AppendLine("CLIENT ID: " + ClientId, true);
+            mainWindow.AppendLine("Subscribed to " + SubTopic, true);
+        }
+
+
+        private void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e) {
+            var incommingJson = JsonConvert.DeserializeObject<Dictionary<string, object>>(Encoding.UTF8.GetString(e.Message, 0, e.Message.Length));
+
+            // Print received message to window
+            mainWindow.AppendLine(DateTime.Now + "  Message arrived.  Topic: " + e.Topic + "  Message: "
+                    + DatabaseHandler.jsonToStr(incommingJson) + "\n", true);
+
+            var returnJson = incomingMessageProcessor(incommingJson);
+            returnJson["computerName"] = Environment.MachineName;
+
+            // Print outgoing message to window
+            mainWindow.AppendLine(DateTime.Now + "  Message outgoing:" + DatabaseHandler.jsonToStr(returnJson) + "\n", true);
+
+            // Publish return message
+            mqttClient.Publish(PubTopic, Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(returnJson)));
+        }
+
+        /// <summary>
+        /// This method directs incoming mqtt messages to be further processed.
+        /// </summary>
+        /// <param name="directive">Enum-like string that directs what to do with messages.</param>
+        /// <param name="messages">Contents of message.</param>
+        private Dictionary<string, string> incomingMessageProcessor(Dictionary<String, object> messages) {
+            Dictionary<string, string> returnMessage = null;
+            try {
+                switch ((string)messages["directive"]) {
+                    case "GET_NEW_BLU":
+                        returnMessage = NavigationHandler.getNewBlu(messages);
+                        break;
+                    case "COMPLETE_NEW_BLU":
+                        returnMessage = NavigationHandler.completeNewBlu(messages);
+                        break;
+                    case "GET_NEW_SLT":
+                        returnMessage = NavigationHandler.getNewSlt(messages);
+                        break;
+                    case "COMPLETE_NEW_SLT":
+                        returnMessage = NavigationHandler.completeNewSlt(messages);
+                        break;
+                    case "GET_DONE_BLU":
+                        returnMessage = NavigationHandler.getDoneBlu(messages);
+                        break;
+                    case "COMPLETE_DONE_BLU":
+                        returnMessage = NavigationHandler.completeDoneBlu(messages);
+                        break;
+                    default:
+                        Console.Error.WriteLine("incomingMessageProcessor: Directive unrecognized.");
+                        break;
+                }
+            }
+            catch (Exception e) {
+                returnMessage = new Dictionary<string, string> { ["error"] = e.Message };
+            }
+            return returnMessage;
+        }
+
+        public void Disconnect() {
+            mqttClient.Disconnect();
+        }
+    }
+}
