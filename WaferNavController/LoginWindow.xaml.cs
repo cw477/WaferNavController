852c276d165812e41fd0cf6d432d602858ef4e9b
jodyn <jnietz@uci.edu>
2017-05-18 17:48:12 -0700

    Added new login window, removed retired add/update section, removed logo and title from all tabs, and other minor UI edits
    Added new login window. Removed retired add/update section from Config
    tab. Removed company logo and title text off all tabs and added it to
    the new login window. Changed "Set" button text to "Import". Changed
    title text to "ActivATE WaferNav Controller". Other minor edits for
    appearance or consistency.

---
+++ WaferNavController/LoginWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WaferNavController
+{
+    /// <summary>
+    /// Interaction logic for LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : BaseWindow
+    {
+        
+        private void LoginButton_Clicked(object sender, RoutedEventArgs e)
+        {
+            
+        }
+        
+    }
+}
