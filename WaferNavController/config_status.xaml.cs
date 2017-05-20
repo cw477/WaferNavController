75e67050e51d6842aec9999aec92526a5ad61e54
Brian Chipman <namedtuple@gmail.com>
2017-05-20 12:19:16 -0700

    Cleaned up unused methods in config_status.

--- WaferNavController/config_status.xaml.cs
+++ WaferNavController/config_status.xaml.cs
@@ -1,18 +1,9 @@
 using System;
-using System.Collections.Generic;
 using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using Microsoft.Win32;
 using System.IO;
 using Path = System.IO.Path;
@@ -44,25 +35,6 @@ namespace WaferNavController
                 string filename = sfd.FileName;
                 File.WriteAllText(filename, logBox.Text.Replace("\n", "\r\n"));
             }  
-
-        }
-        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
-        {
-
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
-        }
-
-        private void TextBox_TextChanged_2(object sender, TextChangedEventArgs e)
-        {
 
         }
 
@@ -100,10 +72,6 @@ namespace WaferNavController
             AddWindow addWindow = new AddWindow(this);
             addWindow.Owner = mainWindow;
             addWindow.ShowDialog();
-        }
-
-        private void SaveButton_Click(object sender, RoutedEventArgs e) {
-            mainWindow.AppendLine("SaveButton_Click called.", true);
         }
 
         private void DataGrid_CellEditEnding(object sender, DataGridCellEditEndingEventArgs e) {
@@ -192,7 +160,7 @@ namespace WaferNavController
                 mainWindow.AppendLine("Successfully reset database with data from imported config file!", true);
                 TabControl.SelectedIndex = 0;
             }
-            catch (Exception ex) {
+            catch (Exception) {
                 mainWindow.AppendLine("Failed to load reset database with config file data!", true);
             }
         }
